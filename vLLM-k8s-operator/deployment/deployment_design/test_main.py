706cd117b62ace944261b346456b67585b8b9a9e
Mrzhiyao <47046655+Mrzhiyao@users.noreply.github.com>
2025-08-21 14:58:02 +0800

    Update test_main.py

--- vLLM-k8s-operator/deployment/deployment_design/test_main.py
+++ vLLM-k8s-operator/deployment/deployment_design/test_main.py
@@ -63,59 +63,6 @@ def query_pods_t2v():
         print(f"请求异常: {e}")
         return None
 
-
-# def query_pods_t2v():
-#     try:
-#         # 动态时间窗口（例如：只保留过去5分钟内的活跃数据）
-#         time_threshold = datetime.utcnow() - timedelta(minutes=2)
-#         timestamp_threshold = time_threshold.timestamp()
-
-#         # 修改查询语句：添加时间戳筛选
-#         QUERY = f'''
-#         sum(
-#             container_memory_usage_bytes{{container!="", container!="POD"}}
-#             and on(pod) timestamp(container_memory_usage_bytes) > {timestamp_threshold}
-#         ) by (node, pod, container)
-#         '''
-
-#         response = requests.get(
-#             f"{PROMETHEUS_URL}/api/v1/query",
-#             params={"query": QUERY}
-#         )
-#         response.raise_for_status()
-
-#         data = response.json()
-#         if data["status"] == "success":
-#             results = data["data"]["result"]
-#             container_info = []
-            
-#             for result in results:
-#                 metric = result["metric"]
-#                 try:
-#                     # 提取必要字段（已通过 PromQL 过滤过期数据）
-#                     record = {
-#                         "Hostname": metric["node"],
-#                         "pod": metric["pod"],
-#                         "container": metric["container"],
-#                         "value": float(result["value"][1]),
-#                         "last_seen": datetime.fromtimestamp(float(result["value"][0]))
-#                     }
-#                 except KeyError as e:
-#                     print(f"缺少关键标签 {e}, 原始数据: {metric}")
-#                     continue
-                
-#                 container_info.append(record)
-            
-#             return container_info
-#         else:
-#             print("查询失败:", data.get("error", "未知错误"))
-#             return None
-
-#     except Exception as e:
-#         print(f"请求异常: {e}")
-#         return None
-
-
 def find_gpu_nodes(data, min_gpu):
     """
     基于GPU可用量筛选节点的函数
@@ -201,11 +148,6 @@ def available_ip(threshold):
                 if node['Available_GPU'] <0:
                     node['Available_GPU'] = 0
 
-        # print(f"IP: {node['IP']} | 主机: {node['Hostname']}")
-        # print(f"  可用显存: {node['Available_GPU']}MB (总{node['Total_GPU']}MB)")
-        # print(f"  显存利用率: {node['GPU_Utilization']} | 内存使用: {node['Memory_Status']}\n")
-
-    
     excluded_hostnames = {"b410-4090d-2", "b410-2070s-4"}
     filtered_result = [
         item for item in filtered_result 
@@ -214,19 +156,10 @@ def available_ip(threshold):
     # 屏蔽指定节点192.168.2.190：
     filtered_result = [item for item in filtered_result if item["Hostname"] not in excluded_hostnames]
 
-    # for node in filtered_result:
-    #     if node['IP'] == '192.168.2.190':
-    #         node.update({
-    #             'Available_GPU': 10,
-    #             'GPU_Utilization': '99.9%'
-    #         })
-    #         break  # 找到后立即退出循环
-    
+
 
     # print('ips:', filtered_result)
     return filtered_result
-
-
 
 from kubernetes import client, config
 
@@ -251,37 +184,3 @@ def check_pod_status(pod_name, namespace="default"):
         else:
             return False, f"API 错误: {e.reason}"
 
-# available_ips = available_ip(5000)
-# sorted_available_ips = sorted(available_ips, key=lambda x: x['Total_GPU'], reverse=True)
-# hostnames = [node['Hostname'] for node in sorted_available_ips]
-# print(hostnames)
-
-# monitor = ClusterMonitor()
-# new_gpu_resoure, new_memory_resource,stats = monitor.run_monitor()
-# # print(new_gpu_resoure, new_memory_resource)
-
-# # print(stats)
-
-# # 使用示例：查找可用GPU≥5000MB的节点
-# threshold = 5000
-# result = find_gpu_nodes(stats, threshold)
-# service_develpoment = query_pods_t2v()
-# filtered = [item for item in service_develpoment if item['pod'] != '']
-# filtered_text2video_items = [
-#     item for item in filtered
-# if "text2video"  in item.get("pod", "").lower()
-# ]
-# # 提前预留的gpu需要删去
-# print(filtered_text2video_items)
-# print(result)
-# exclude_hostnames = {item['Hostname'] for item in filtered_text2video_items}
-
-# # 过滤list1中不包含这些hostname的条目
-# filtered_result = [item for item in result if item['Hostname'] not in exclude_hostnames]
-
-# # 格式化输出
-# print(f"可用GPU≥{threshold}MB的节点:")
-# for node in filtered_result:
-#     print(f"IP: {node['IP']} | 主机: {node['Hostname']}")
-#     print(f"  可用显存: {node['Available_GPU']}MB (总{node['Total_GPU']}MB)")
-#     print(f"  显存利用率: {node['GPU_Utilization']} | 内存使用: {node['Memory_Status']}\n")
