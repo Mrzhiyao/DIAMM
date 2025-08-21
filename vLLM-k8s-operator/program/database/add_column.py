ad20bf4a712d292ab62b9d8a80b7710f46d25db5
Mrzhiyao <47046655+Mrzhiyao@users.noreply.github.com>
2025-08-21 15:52:12 +0800

    Create add_column.py

---
+++ vLLM-k8s-operator/program/database/add_column.py
@@ -0,0 +1,35 @@
+from sqlalchemy import MetaData, Table, text, create_engine
+
+DATABASE_URL = "postgresql+psycopg2://postgres:TaskingAI321@192.168.2.75:5432/coco"
+
+# 创建数据库引擎
+engine = create_engine(DATABASE_URL, echo=True)
+
+# 初始化 MetaData 并绑定引擎
+metadata = MetaData(schema="coco")
+metadata.reflect(bind=engine)  # 使用 reflect 方法加载表信息
+
+# 加载表结构
+tasks_table = Table('tasks', metadata, autoload_with=engine)
+
+# 检查并添加列
+if 'file_path' not in tasks_table.c:  # 检查列是否已存在
+    with engine.connect() as conn:
+        alter_table_query = text('ALTER TABLE coco.tasks ADD COLUMN file_path VARCHAR;')
+        conn.execute(alter_table_query)  # 执行 SQL
+        conn.commit()  # 显式提交事务
+        print("列 file_path 已成功添加到 tasks 表")
+
+else:
+    print("列 file_path 已存在，无需添加")
+
+# 检查并添加 vector 列  
+if 'vector' not in tasks_table.c:  # 检查列是否已存在  
+    with engine.connect() as conn:  
+        alter_table_query = text('ALTER TABLE coco.tasks ADD COLUMN vector VECTOR(768);')  
+        conn.execute(alter_table_query)  # 执行 SQL  
+        conn.commit()  # 显式提交事务  
+        print("列 vector 已成功添加到 tasks 表")  
+else:  
+    print("列 vector 已存在，无需添加") 
+
